107b4d31b4bbdb9abca321d8a0e73311178c3188
xyx22 <oleg.gudechek@mail.ru>
2024-08-11 23:39:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+The_number_of_completed_DZ = "12"
+The_number_of_hours_spent = "1.5"
+Course_name = "Python"
+print(1.5/12)
+print(Course_name, "всего задач:", The_number_of_completed_DZ, "затрачено часов:", The_number_of_hours_spent, "среднее время выполнения 0.125", ".")
+print(Course_name, "всего задач:", The_number_of_completed_DZ, "затрачено часов:", The_number_of_hours_spent, "среднее время выполнения", 1.5/12,  ".")
+print(The_number_of_completed_DZ) * print(The_number_of_hours_spent) # TimeForOneTask
+print(The_number_of_completed_DZ [:]) * print(The_number_of_hours_spent[:]) # TimeForOneTask
+print(The_number_of_completed_DZ [:],* The_number_of_hours_spent [:] ) # TimeForOneTask подскажите как благодаря названию строки умножить на еще название строки и чтобы умножалось только значение строки
+# хочу автоматизировать данное дейстрие 2 строчку разделить на 1. Если есть возможеость ответ дайте в телеграмме 
