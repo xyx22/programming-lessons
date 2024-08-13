a8b5e506316e062697859ca0b6d45e46ed7c2656
xyx22 <oleg.gudechek@mail.ru>
2024-08-13 22:41:34 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-The_number_of_completed_DZ = "12"
-The_number_of_hours_spent = "1.5"
-Course_name = "Python"
-print(1.5/12)
-print(Course_name, "всего задач:", The_number_of_completed_DZ, "затрачено часов:", The_number_of_hours_spent, "среднее время выполнения 0.125", ".")
-print(Course_name, "всего задач:", The_number_of_completed_DZ, "затрачено часов:", The_number_of_hours_spent, "среднее время выполнения", 1.5/12,  ".")
-print(The_number_of_completed_DZ) * print(The_number_of_hours_spent) # TimeForOneTask
-print(The_number_of_completed_DZ [:]) * print(The_number_of_hours_spent[:]) # TimeForOneTask
-print(The_number_of_completed_DZ [:],* The_number_of_hours_spent [:] ) # TimeForOneTask подскажите как благодаря названию строки умножить на еще название строки и чтобы умножалось только значение строки
-# хочу автоматизировать данное дейстрие 2 строчку разделить на 1. Если есть возможеость ответ дайте в телеграмме 
+a = "example" # 1
+print(a[:]) # всю строку
+print(a[0]) # 2
+print(a[-1]) # 3
+print(a[4:]) # 4
+print(a[::-1]) # 5
+print(a[::-1][2])
+print(a[0:7:2]) # 6
