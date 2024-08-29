332af1565a5ba1d2241721b4308d4cfbda61b82e
xyx22 <oleg.gudechek@mail.ru>
2024-08-29 23:16:04 +0300

    Update module_1_5.py.py

--- module_1_5.py.py
+++ module_1_5.py.py
@@ -1,9 +1,9 @@
 immutable_var = 15, 72, 64, 'Один'
 print(immutable_var)
 #tuple[0] = 20 # не поддерживает изменение по элементу но если
-mutable_list = [15], 72, [15], ['Один']
-mutable_list[2][0] = 20 #ТОГДА ПОЛУЧАЕТСЯ
-mutable_list[0][0] = 20
-mutable_list[3][0] = 'Одиннадцать'
+mutable_list = [1, 20, "15", "72","Один"]
+mutable_list[0] = 20 #ТОГДА ПОЛУЧАЕТСЯ
+mutable_list[0] = 20
+mutable_list[1] = 'Одиннадцать'
 print(mutable_list)
 #mutable_list[0][0] = 'Одиннадцать'
