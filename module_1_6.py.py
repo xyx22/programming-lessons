b38ff4b3bc5f5a60fa84b77b01f9ed719f638c38
xyx22 <oleg.gudechek@mail.ru>
2024-09-02 22:45:54 +0300

    Add files via upload

---
+++ module_1_6.py.py
@@ -0,0 +1,21 @@
+my_dict = {"Олег": 2002, "Иван": 2002}
+print(my_dict)
+print(my_dict.get("Олег"))
+print(my_dict.get("Никита"))
+my_dict = {"Олег": 2002, "Иван": 2002}
+print(my_dict)
+my_dict["Макс"]= 1999 #
+print(my_dict)
+my_dict.update({"Инна": 1998, "Артур" : 1999})
+print(my_dict)
+del my_dict["Олег"]
+print(my_dict)
+# Множество
+my_set = {11, 2, 30, 5, 2, "String", "Урбан", (11, 2, 30)}
+print(my_set)
+print(my_set.add(540))
+print(my_set.add(200))
+print(my_set.add(200))
+print(my_set)
+print(my_set.discard(200))
+print(my_set)
