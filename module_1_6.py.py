d0a96dbeef348fcf6692343bec95fbe1b2503fa7
xyx22 <oleg.gudechek@mail.ru>
2024-09-04 23:26:04 +0300

    Update module_1_6.py.py

--- module_1_6.py.py
+++ module_1_6.py.py
@@ -8,7 +8,9 @@ my_dict["Макс"]= 1999 #
 print(my_dict)
 my_dict.update({"Инна": 1998, "Артур" : 1999})
 print(my_dict)
-del my_dict["Олег"]
+a = my_dict.pop("Олег", "Инна") # не получается больше одного
+print(a)
+#del my_dict["Олег"]
 print(my_dict)
 # Множество
 my_set = {11, 2, 30, 5, 2, "String", "Урбан", (11, 2, 30)}
