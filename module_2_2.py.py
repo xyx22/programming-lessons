e96ba5d40c7ddd7006a653f89fcc89f09f0a045e
xyx22 <oleg.gudechek@mail.ru>
2024-09-10 23:21:45 +0300

    Add files via upload

---
+++ module_2_2.py.py
@@ -0,0 +1,30 @@
+first = 423
+second = 723
+third = 423
+if first == second and first == third:
+    print("3")
+elif first == second or first == third:
+    print("2")
+else:
+    print("0")
+print("Вариант 1")
+first = 423
+second = 723
+third = 475
+if first == second and first == third:
+    print("3")
+elif first == second or first == third:
+    print("2")
+else:
+    print("0")
+print("Вариант 2")
+first = 475
+second = 475
+third = 475
+if first == second and first == third:
+    print("3")
+elif first == second or first == third:
+    print("2")
+else:
+    print("0")
+print("Вариант 2")
