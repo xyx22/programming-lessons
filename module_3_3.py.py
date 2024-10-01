2d8cc0de41c4dd120057118b5cd9fb967df863aa
xyx22 <oleg.gudechek@mail.ru>
2024-10-01 23:18:21 +0300

    Add files via upload

---
+++ module_3_3.py.py
@@ -0,0 +1,17 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+values_list = [12 , 'twelve', True]
+values_dict = {'a': 'привет','b': 12, 'c' : False }
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = 1, 'twelve'
+print_params(*values_list_2, 42)
+
+values_list_2 = [54.32, 'Строка' ]
+print_params(*values_list_2, 42)
