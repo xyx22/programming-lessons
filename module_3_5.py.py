c9170b5a32300db118caf966f74182ef25a887e1
xyx22 <oleg.gudechek@mail.ru>
2024-10-07 21:20:41 +0300

    Add files via upload

---
+++ module_3_5.py.py
@@ -0,0 +1,16 @@
+from calendar import firstweekday
+
+
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    elif first != 0:
+        return first
+    else:
+        return 1
+result = get_multiplied_digits(40203)
+print(result)
+
+
